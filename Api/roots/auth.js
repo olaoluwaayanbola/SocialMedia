66e3af248b73378db33ff8d204ade0e01575639b
olaoluwa <jacobaromiwe@gmail.com>
2023-01-14 20:51:25 +0000

    Update auth.js

--- Api/roots/auth.js
+++ Api/roots/auth.js
@@ -1,7 +1,18 @@
 const express = require("express")
+const userModel = require("../models/users")
 const Router = express.Router()
 
-Router.get("/",(req,res) => {
-    res.send('hello')
+// Register User
+Router.get("/register", async (req,res) => {
+    try{
+        const User = await new User({
+        username:"james",
+        email:"james@gmail.com",
+        password:"12345"
+    })}catch(err){
+        console.log(err)
+    }
+    await User.save()
+    res.send("ok")
 })
 module.exports = Router
