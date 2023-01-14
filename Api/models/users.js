ca5b15e680dcea5734b2995105ad930e39df0466
olaoluwa <jacobaromiwe@gmail.com>
2023-01-14 20:46:23 +0000

    Update users.js

--- Api/models/users.js
+++ Api/models/users.js
@@ -1,1 +1,49 @@
 const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        min:3,
+        max:20,
+        unique:true
+    },
+    email:{
+        type:String,
+        require:true,
+        min:5,
+        max:30,
+        unique:true
+    },
+    password:{
+        type:Number||String,
+        require:true,
+        min:3,
+        unique:true
+    },
+    profilePicture:{
+        type:String,
+        default:""
+    },
+    coverPicture:{
+        type:String,
+        default:""
+    },
+    followers:{
+        type:Array,
+        default:[]
+    },
+    following:{
+        type:Array,
+        default:[]
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false,
+    },
+    {
+        timestamps:true
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
