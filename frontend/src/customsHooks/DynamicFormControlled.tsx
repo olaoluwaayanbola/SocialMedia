ffd1ea5e31a8fb9f71d6a9e561569e2cd8db4ca9
olaoluwa <jacobaromiwe@gmail.com>
2023-01-21 13:02:37 +0000

    Update 4 files

--- frontend/src/customsHooks/DynamicFormControlled.tsx
+++ frontend/src/customsHooks/DynamicFormControlled.tsx
@@ -1,16 +1,14 @@
 import { useState } from "react"
 
-export const useForm = (initialStates) => {
+export const useForm = (initialStates:object) => {
     const [form,setForm] = useState<any>(initialStates)
     
     const HandleForm = (event:any):void => {
         const { type, name, value, checked} = event.target
-        setForm((prevValues) => {
-            return(
-                ...prevValues,
-                [name]: type === "checkbox" ? checked : value
-            )
-        }) 
+        setForm((prevValues:any) => ({
+            ...prevValues,
+            [name]:value
+        })) 
     }
-    return { form,HandleForm }
+    return ( {form,HandleForm} )
 }
