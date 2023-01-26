8232487330eff3727a3bb4efb82396801ca95ad0
olaoluwa <jacobaromiwe@gmail.com>
2023-01-26 06:34:52 +0000

    Update 3 files

---
+++ frontend/src/Pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+import style from "./Home.module.scss"
+import TopBarNavigation from "../../components/Navigations/Navigation"
+
+const Home = ( ) => {
+    return(
+        <div className={style.Contianer}>
+            <TopBarNavigation/>
+            <asi>
+            <main>
+                
+            </main>
+        </div>
+    )
+}
+export default Home
